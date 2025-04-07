1973daf461c95b8498674107993c5d3b57990c15
Sergio Heredia Carmona <sergiohercar1@gmail.com>
2025-04-07 13:20:10 +0200

    ADD bathymetry logic

---
+++ bathymetry.py
@@ -0,0 +1,68 @@
+from typing import Tuple
+import numpy as np
+from data_types import CalibrationSummary, ValidationSummary
+
+def pseudomodel_green(blue : np.ndarray, green : np.ndarray, n : float) -> np.ndarray:
+    return np.log(blue * n) / np.log(green * n)
+
+def pseudomodel_red(blue : np.ndarray, red : np.ndarray, n : float) -> np.ndarray:
+    return np.log(blue * n) / np.log(red * n)
+
+def get_stratified_depths(depths : np.ndarray, n_segments : int = 10, sample_size_per_depth : int = 5, min_depth = None, max_depth = None, random = None) -> np.ndarray:
+    min_depth = np.nanmin(depths) if min_depth is None else min_depth
+    max_depth = np.nanmax(depths) if max_depth is None else max_depth
+    
+    segments = np.linspace(min_depth, max_depth, n_segments)
+    indexes = np.arange(depths.size)
+
+    selected_indexes = []
+    for idx, depth in enumerate(segments[:-1]):
+        min_depth = depth
+        max_depth = segments[idx + 1]
+
+        to_select = (min_depth <= depths) & (depths <= max_depth)
+
+        if random is None:
+            selected_indexes.extend(np.random.choice(indexes[to_select], sample_size_per_depth, 
+                                                 replace = False))
+        else:
+            selected_indexes.extend(random.choice(indexes[to_select], sample_size_per_depth, 
+                                                 replace = False))
+
+    return np.array(selected_indexes)
+
+def calibrate(pseudomodel : np.ndarray, in_situ : np.ndarray, lon : np.ndarray, lat : np.ndarray) -> CalibrationSummary:
+    r_square, m, n = do_linear_regression(pseudomodel, in_situ)
+    
+    return CalibrationSummary(pseudomodel, in_situ, lon, lat, r_square, m, n)
+
+def do_linear_regression(x : np.ndarray, y : np.ndarray) -> Tuple[float, float, float]:
+    m, n = np.polyfit(x, y, 1)
+    r_square = np.corrcoef(x, y)[0, 1] ** 2
+
+    return r_square, m, n
+
+
+def validate(model : np.ndarray, in_situ : np.ndarray) -> ValidationSummary:
+    return ValidationSummary(model, in_situ)
+    
+
+def composite(green_pseudomodels : np.ndarray, red_pseudomodels : np.ndarray) -> np.ndarray:
+    return np.nanmax(green_pseudomodels, axis = 0), np.nanmax(red_pseudomodels, axis = 0), np.argmax(green_pseudomodels, axis = 0)
+
+
+def switching_model(green_model : np.ndarray, red_model : np.ndarray, green_coef : float = 3.5, red_coef : float = 2) -> np.ndarray:
+    # Equations
+    a = (green_coef - red_model) / (green_coef - red_coef)
+    b = 1 - a
+    SDBw = a * red_model + b * green_model
+
+    SDB = np.zeros(red_model.shape)
+    SDB[:,:] = np.nan
+
+    SDB = np.where(red_model < red_coef, red_model, SDB)
+    SDB = np.where((red_model > red_coef) & (green_model > green_coef), green_model, SDB)
+    SDB = np.where((red_model >= red_coef) & (green_model <= green_coef), SDBw, SDB)
+    SDB[SDB < 0] = np.nan
+    
+    return SDB
