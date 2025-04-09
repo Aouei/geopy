19d756b0174add8fad46121903a9b8b7c5674a78
Sergio Heredia Carmona <sergiohercar1@gmail.com>
2025-04-10 01:41:13 +0200

    UPDATE bathymetry file cleaning

--- bathymetry.py
+++ bathymetry.py
@@ -7,41 +7,17 @@ from data_types import CalibrationSummary, ValidationSummary
 def stumpf_pseudomodel(blue : np.ndarray, other : np.ndarray, n : float = np.pi * 1_000):
     return np.log(blue * n) / np.log(other * n)
 
-def get_stratified_depths(depths : np.ndarray, n_segments : int = 10, sample_size_per_depth : int = 5, min_depth = None, max_depth = None, random = None) -> np.ndarray:
-    min_depth = np.nanmin(depths) if min_depth is None else min_depth
-    max_depth = np.nanmax(depths) if max_depth is None else max_depth
-    
-    segments = np.linspace(min_depth, max_depth, n_segments)
-    indexes = np.arange(depths.size)
+def multi_image(p_Greens : np.ndarray, p_Reds : np.ndarray) -> np.ndarray:
+    return np.nanmax(p_Greens, axis = 0), np.nanmax(p_Reds, axis = 0), \
+            np.argmax(p_Greens, axis = 0)
 
-    selected_indexes = []
-    for idx, depth in enumerate(segments[:-1]):
-        min_depth = depth
-        max_depth = segments[idx + 1]
 
-        to_select = (min_depth <= depths) & (depths <= max_depth)
-
-        if random is None:
-            selected_indexes.extend(np.random.choice(indexes[to_select], sample_size_per_depth, 
-                                                 replace = False))
-        else:
-            selected_indexes.extend(random.choice(indexes[to_select], sample_size_per_depth, 
-                                                 replace = False))
-
-    return np.array(selected_indexes)
-
-def calibrate(pseudomodel : np.ndarray, in_situ : np.ndarray, lon : np.ndarray, lat : np.ndarray) -> CalibrationSummary:
-    slope, intercept, r_value, *_ = scipy.stats.linregress(pseudomodel, in_situ)
-    return CalibrationSummary(pseudomodel, in_situ, lon, lat, r_value ** 2, slope, intercept)
-
+def calibrate(p_model : np.ndarray, in_situ : np.ndarray, lon : np.ndarray, lat : np.ndarray) -> CalibrationSummary:
+    slope, intercept, r_value, *_ = scipy.stats.linregress(p_model, in_situ)
+    return CalibrationSummary(p_model, in_situ, lon, lat, r_value ** 2, slope, intercept)
 
 def validate(model : np.ndarray, in_situ : np.ndarray) -> ValidationSummary:
     return ValidationSummary(model, in_situ)
-    
-
-def multi_image(green_pseudomodels : np.ndarray, red_pseudomodels : np.ndarray) -> np.ndarray:
-    return np.nanmax(green_pseudomodels, axis = 0), np.nanmax(red_pseudomodels, axis = 0), \
-            np.argmax(green_pseudomodels, axis = 0)
 
 
 def switching_model(green_model : np.ndarray, red_model : np.ndarray, green_coef : float = 3.5, red_coef : float = 2) -> np.ndarray:
