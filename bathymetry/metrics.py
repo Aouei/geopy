1583e3eb1404e308396bc52922c3d1b067f918f9
Sergio Heredia Carmona <sergiohercar1@gmail.com>
2025-04-12 04:03:26 +0200

    UPDATE bathymetry code-design

--- bathymetry/metrics.py
+++ bathymetry/metrics.py
@@ -1,43 +1,7 @@
-from __future__ import annotations
 import numpy as np
-import pandas as pd
 
-from typing import Tuple
 from dataclasses import dataclass
 
-def do_linear_regression(x : np.ndarray, y : np.ndarray) -> Tuple[float, float, float]:
-    m, n = np.polyfit(x, y, 1)
-    r_square = np.corrcoef(x, y)[0, 1] ** 2
-
-    return r_square, m, n
-
-@dataclass
-class CalibrationSummary():
-    pseudomodel : np.ndarray
-    in_situ : np.ndarray
-    lon : np.ndarray
-    lat : np.ndarray
-    r_square : float
-    m : float
-    n : float
-    name : str = ''
-
-    def predict(self, x : np.ndarray) -> np.ndarray:
-        return self.m * x + self.n
-    
-    def __str__(self) -> str:
-        return f'N: {len(self.in_situ)} | R: {self.r_square:.4f} | y = {self.m:.3f}x{self.n:+.3f} | Name {self.name}'
-    
-    def __repr__(self) -> str:
-        return str(self)
-
-    @staticmethod
-    def load(filename : str) -> CalibrationSummary:
-        df = pd.read_csv(filename)
-        r_square, m, n = do_linear_regression(df.pseudomodel, df.in_situ)
-
-        return CalibrationSummary(df.pseudomodel.values, df.in_situ.values, df.lon.values, 
-                                  df.lat.values, r_square, m, n)
 
 @dataclass
 class ValidationSummary():
---
+++ bathymetry/models.py
@@ -0,0 +1,67 @@
+import numpy as np
+import scipy
+
+from bathymetry.metrics import ValidationSummary
+
+
+def stumpf_pseudomodel(blue : np.ndarray, other : np.ndarray, n : float = np.pi * 1_000):
+    return np.log(blue * n) / np.log(other * n)
+
+def multi_image_pseudomodel(p_greens : np.ndarray, p_reds : np.ndarray) -> np.ndarray:
+    return np.nanmax(p_greens, axis = 0), np.nanmax(p_reds, axis = 0), np.argmax(p_greens, axis = 0)
+
+
+def switching_model(green_model : np.ndarray, red_model : np.ndarray, green_coef : float = 3.5, red_coef : float = 2) -> np.ndarray:
+    a = (green_coef - red_model) / (green_coef - red_coef)
+    b = 1 - a
+    switching_model = a * red_model + b * green_model
+
+    model = np.zeros(red_model.shape)
+    model[:] = np.nan
+
+    model = np.where(red_model < red_coef, red_model, model)
+    model = np.where((red_model > red_coef) & (green_model > green_coef), green_model, model)
+    model = np.where((red_model >= red_coef) & (green_model <= green_coef), switching_model, model)
+    model[model < 0] = np.nan
+    
+    return model
+
+
+class LinearModel(object):
+    def __init__(self, name : str) -> None:
+        self.lon = None
+        self.lat = None
+        self.pseudomodel = None
+        self.in_situ = None
+        self.name = name
+
+    def fit(self, pseudomodel : np.ndarray, in_situ : np.ndarray, save : bool = False) -> None:
+        self._set_linear_regression(pseudomodel, in_situ)
+
+        if save:
+            self.pseudomodel = pseudomodel
+            self.in_situ = in_situ
+
+    def _set_linear_regression(self, X : np.ndarray, y : np.ndarray) -> np.ndarray:
+        slope, intercept, r_value, *_ = scipy.stats.linregress(X, y)
+        self.slope = slope
+        self.intercept = intercept
+        self.r_square = r_value ** 2
+
+    def predict(self, pseudomodel : np.ndarray) -> np.ndarray:
+        return self.slope * pseudomodel + self.intercept
+    
+    def predict_and_evaluate(self, pseudomodel : np.ndarray, in_situ : np.ndarray) -> ValidationSummary:
+        return ValidationSummary(self.predict(pseudomodel), in_situ)
+    
+    def to_csv(self, outname : str) -> None: # TODO
+        pass
+
+    def read_csv(self, filename : str) -> None: # TODO
+        pass
+
+    def __str__(self) -> str:
+        return f'N: {len(self.in_situ)} | R: {self.r_square:.4f} | y = {self.slope:.3f}x{self.intercept:+.3f} | Name {self.name}'
+    
+    def __repr__(self) -> str:
+        return str(self)
