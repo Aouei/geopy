5cef7a5205153444e1870efe32f2e4b7c17dd5ad
Sergio Heredia Carmona <sergiohercar1@gmail.com>
2025-04-21 12:01:43 +0200

    ADD docstring

--- preprocessing/deglinting.py
+++ preprocessing/deglinting.py
@@ -4,6 +4,18 @@ from scipy.stats import mode
 
 
 def hedley(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, nir_band : int) -> np.ndarray:
+    """Hedley method for deglinting.
+
+    Args:
+        deep_area (np.ndarray): Area to use for correction
+        to_correct (np.ndarray): bands to correct
+        nir (np.ndarray): nir band values
+        nir_band (int): nir index in deep area
+
+    Returns:
+        np.ndarray: to_correct with deglinting applied
+    """
+
     no_nans = ~np.isnan(deep_area[nir_band])
 
     for idx in range(len(to_correct)):
@@ -15,6 +27,18 @@ def hedley(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, ni
 
 
 def lyzenga(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, nir_band : int) -> np.ndarray:
+    """Lyzenga method for deglinting.
+
+    Args:
+        deep_area (np.ndarray): Area to use for correction
+        to_correct (np.ndarray): bands to correct
+        nir (np.ndarray): nir band values
+        nir_band (int): nir index in deep area
+
+    Returns:
+        np.ndarray: to_correct with deglinting applied
+    """
+
     no_nans = ~np.isnan(deep_area[nir_band])
     
     for idx in range(len(to_correct)):
@@ -26,6 +50,18 @@ def lyzenga(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, n
 
 
 def joyce(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, nir_band : int) -> np.ndarray:
+    """Joyce method for deglinting.
+
+    Args:
+        deep_area (np.ndarray): Area to use for correction
+        to_correct (np.ndarray): bands to correct
+        nir (np.ndarray): nir band values
+        nir_band (int): nir index in deep area
+
+    Returns:
+        np.ndarray: to_correct with deglinting applied
+    """
+
     no_nans = ~np.isnan(deep_area[nir_band])
     
     for idx in range(len(to_correct)):
