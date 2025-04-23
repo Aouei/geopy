2b7816eada89232bcdc75b22ef25d0f10381c23d
Sergio Heredia Carmona <sergiohercar1@gmail.com>
2025-04-23 12:22:53 +0200

    ADD index.html

---
+++ docs/html/_sources/modules/selector.rst.txt
@@ -0,0 +1,9 @@
+Selector Module
+=============
+
+The Selector module provides functionality for selecting data points from arrays based on value intervals.
+
+.. automodule:: selector
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,3 +18,6 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_extra_path = ['.nojekyll']
+html_baseurl = 'https://aouei.github.io/geopy/'
