47c35e57e51da90c54ce49cc38536d6f26013e9c
Sergio Heredia Carmona <sergiohercar1@gmail.com>
2025-04-24 09:41:48 +0200

    UPDATE deglint methods are more intuitive

--- geopy/preprocessing/deglinting.py
+++ geopy/preprocessing/deglinting.py
@@ -3,70 +3,73 @@ import numpy as np
 from scipy.stats import mode
 
 
-def hedley(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, nir_band : int) -> np.ndarray:
+def hedley(deep_area_mask : np.ndarray, to_correct : np.ndarray, nir : np.ndarray) -> np.ndarray:
     """Hedley method for deglinting.
 
     Args:
         deep_area (np.ndarray): Area to use for correction
         to_correct (np.ndarray): bands to correct
         nir (np.ndarray): nir band values
-        nir_band (int): nir index in deep area
 
     Returns:
         np.ndarray: to_correct with deglinting applied
     """
 
-    no_nans = ~np.isnan(deep_area[nir_band])
-
     for idx in range(len(to_correct)):
-        m = np.polyfit(deep_area[nir_band][no_nans].ravel(), deep_area[idx][no_nans].ravel(), 1)[0]
-        to_correct[idx] = to_correct[idx] - m * (nir - np.nanmin(deep_area[nir_band]))
+        deep_value = to_correct[idx][deep_area_mask]
+        deep_nir = nir[deep_area_mask]
+        is_valid = ~np.isnan(deep_value) & ~np.isnan(deep_nir)
+
+        m = np.polyfit(deep_nir[is_valid].ravel(), deep_value[is_valid].ravel(), 1)[0]
+        to_correct[idx] = to_correct[idx] - m * (nir - np.nanmin(deep_nir[is_valid]))
         to_correct[idx][to_correct[idx] < 0] = np.nan
     
     return to_correct
 
 
-def lyzenga(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, nir_band : int) -> np.ndarray:
+def lyzenga(deep_area_mask : np.ndarray, to_correct : np.ndarray, nir : np.ndarray) -> np.ndarray:
     """Lyzenga method for deglinting.
 
     Args:
-        deep_area (np.ndarray): Area to use for correction
+        deep_area_mask (np.ndarray): Area to use for correction
         to_correct (np.ndarray): bands to correct
         nir (np.ndarray): nir band values
-        nir_band (int): nir index in deep area
 
     Returns:
         np.ndarray: to_correct with deglinting applied
     """
 
-    no_nans = ~np.isnan(deep_area[nir_band])
-    
     for idx in range(len(to_correct)):
-        m = np.cov(deep_area[nir_band][no_nans].ravel(), deep_area[idx][no_nans].ravel())[0, 1]
-        to_correct[idx] = to_correct[idx] - m * (nir - np.nanmean(deep_area[nir_band]))
+        deep_value = to_correct[idx][deep_area_mask]
+        deep_nir = nir[deep_area_mask]
+        is_valid = ~np.isnan(deep_value) & ~np.isnan(deep_nir)
+
+        m = np.cov(deep_nir[is_valid].ravel(), deep_value[is_valid].ravel())[0, 1]
+        to_correct[idx] = to_correct[idx] - m * (nir - np.nanmean(deep_nir[is_valid]))
         to_correct[idx][to_correct[idx] < 0] = np.nan
     
     return to_correct
 
 
-def joyce(deep_area : np.ndarray, to_correct : np.ndarray, nir : np.ndarray, nir_band : int) -> np.ndarray:
+def joyce(deep_area_mask : np.ndarray, to_correct : np.ndarray, nir : np.ndarray) -> np.ndarray:
     """Joyce method for deglinting.
 
     Args:
-        deep_area (np.ndarray): Area to use for correction
+        deep_area_mask (np.ndarray): Area to use for correction
         to_correct (np.ndarray): bands to correct
         nir (np.ndarray): nir band values
-        nir_band (int): nir index in deep area
 
     Returns:
         np.ndarray: to_correct with deglinting applied
     """
 
-    no_nans = ~np.isnan(deep_area[nir_band])
-    
     for idx in range(len(to_correct)):
-        m = np.polyfit(deep_area[nir_band][no_nans].ravel(), deep_area[idx][no_nans].ravel(), 1)[0]
-        to_correct[idx] = to_correct[idx] - m * (nir - mode(deep_area[nir_band][no_nans].ravel()).mode)
+        deep_value = to_correct[idx][deep_area_mask]
+        deep_nir = nir[deep_area_mask]
+        is_valid = ~np.isnan(deep_value) & ~np.isnan(deep_nir)
+
+        m = np.polyfit(deep_nir[is_valid].ravel(), deep_value[is_valid].ravel(), 1)[0]
+        to_correct[idx] = to_correct[idx] - m * (nir - mode(deep_nir[is_valid].ravel()).mode)
         to_correct[idx][to_correct[idx] < 0] = np.nan
     
     return to_correct
